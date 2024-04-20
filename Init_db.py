80a2acef6ce707191aa42e3f00e7758bbbe9d849
Jenny99-Alca <Jenniferalcantara99@gmail.com>
2024-04-19 23:09:51 -0400

    updates

--- Init_db.py
+++ Init_db.py
@@ -1,4 +1,4 @@
-import sqlite3 
+import sqlite3, json
 
 connection = sqlite3.connect("db.sqlite")
 cursor = connection.cursor()
@@ -18,21 +18,56 @@ CREATE TABLE IF NOT EXISTS items(
 	price REAL NOT NULL
 );
 """)
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS orders(
 	id INTEGER PRIMARY KEY,
-	FOREIGN KEY(cust_id) REFERENCES customers(id),
-    notes TEXT
+    timestamp INTEGER,
+    notes TEXT,
+    cust_id INTEGER,
+	FOREIGN KEY(cust_id) REFERENCES customers(id)
 );
 """)
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS order_list(
     id INTEGER PRIMARY KEY,
-    order_id INTIGER NOT NULL,
+    order_id INTEGER NOT NULL,
     item_id INTEGER NOT NULL,
     FOREIGN KEY(order_id) REFERENCES orders(id),
-    FOREIGN KEY(cust_id) REFERENCES customers(id)
+    FOREIGN KEY(item_id) REFERENCES items(id)
 );
 """)
+
+with open ('example_orders.json') as file:
+    order_list = json.load(file)
+
+orders = []
+customers = {}
+items = {}
+
+for order in order_list:
+    customers[order["phone"]] = order["name"]
+for item in order["items"]:
+    items[item["name"]] = item["price"]
+        
+for phone, name in customers.items():
+	cursor.execute("INSERT INTO customers (name, phone) VALUES (?, ?)", (name,phone))
+     
+for name, price in items.items():
+     cursor.execute("INSERT INTO items (name, price) VALUES (?, ?)", (name,price))
+
+for order in order_list:
+     cursor.execute("SELECT id FROM customers WHERE phone= ?;", (order["phone"],))
+     cust_id = cursor.fetchone()[0]
+     cursor.execute("INSERT INTO orders (notes, timestamp, cust_id) VALUES (?, ?, ?);",
+            (order["notes"], order["timestamp"],cust_id))
+     order_id = cursor.lastrowid
+     
+     for item in order["items"]:
+          cursor.execute("SELECT id FROM items WHERE name=?;",(item["name"],))
+          item_id = cursor.fetchone()[0]
+          cursor.execute("INSERT INTO order_list (order_id, item_id) VALUES (?, ?);",
+                (order_id, item_id))
+
 connection.commit()
 connection.close()
