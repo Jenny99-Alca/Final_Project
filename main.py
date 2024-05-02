ee2fc6b7173bc0bfd3dcdb9214d32f4b3b1a3b74
Jenny99-Alca <Jenniferalcantara99@gmail.com>
2024-05-02 07:13:00 -0400

    New Updates, more debugging needed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import string
 from fastapi import FastAPI, HTTPException
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
@@ -10,14 +11,14 @@ class Customer(BaseModel):
       name: str
       phone: int
 
-@app.post("/customers/")
+@app.post("/customers/{cust_id}")
 def create_customer(customer: Customer):
     if customer.cust_id != None:
         raise HTTPException(status_code=400, detail="cust_id can't be set on post request")
     
     connection = sqlite3.connect("db.sqlite")
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO customers WHERE (name, phone) VALUES (?, ?);",(customer.name, customer.phone))
+    cursor.execute("INSERT INTO customers (name, phone) VALUES (?, ?);", (customer.name, customer.phone))
     customer.cust_id = cursor.lastrowid
     connection.commit()
     connection.close()
@@ -25,17 +26,17 @@ def create_customer(customer: Customer):
     return customer
 
 @app.get("/customers/{cust_id}")
-def read_customer(cust_id: int, q=None):
+def read_customer(cust_id: int, customer: Customer): #def read_customer(cust_id: int, customer: Customer):
     connection = sqlite3.connect("db.sqlite")
     cursor = connection.cursor()
-    cursor.execute("SELECT id FROM customers WHERE phone= ?", (cust_id,))
+    cursor.execute("SELECT id FROM customers WHERE name=?, phone=?", (cust_id,))
     customer = cursor.fetchone()
     connection.close()
 
-    if customer != None:
-            return Customer (cust_id=customer[0], name=customer[1], phone=customer[2])       
+    if customer != None: #if customer.cust_id != None:
+        return Customer (cust_id=customer[0], name=customer[1], phone=customer[2])       
     else:
-         raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail="Customer not found")
     
 @app.put("/customers/{cust_id}")
 def update_customer(cust_id: int, customer: Customer):
@@ -67,10 +68,141 @@ def delete_customer(cust_id: int):
         raise HTTPException(status_code=400, detail="{total_changes} rows were affected")
     return total_changes
 
-#### Items, Orders, and Order List Next
-
-    #update_customer_encoded = jsonable_encoder(customer)
-    #customer[cust_id] = update_customer_encoded
+  #update_customer_encoded = jsonable_encoder(customer)
+  #customer[cust_id] = update_customer_encoded
 
     #return update_customer_encoded
 
+#####Items
+
+class Item(BaseModel):
+      item_id: int | None = None
+      name: str
+      price: int
+
+@app.post("/items/")
+def create_item(item: Item):
+    if item.item_id != None:
+        raise HTTPException(status_code=400, detail="cust_id can't be set on post request")
+    
+    connection = sqlite3.connect("db.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO items (name, price) VALUES (?, ?);",(item.name, item.price))
+    item.item_id = cursor.lastrowid
+    connection.commit()
+    connection.close()
+
+    return item
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q=None):
+    connection = sqlite3.connect("db.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("SELECT id FROM items WHERE price= ?", (item_id,))
+    item = cursor.fetchone()
+    connection.close()
+
+    if item != None:
+            return Item (item_id=item[0], name=item[1], price=item[2])       
+    else:
+         raise HTTPException(status_code=404, detail="Item not found")
+    
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    if item.cust_id != None and item.item_id != item_id:
+        raise HTTPException(status_code=404, detail="Item ID doesn't match ID in Path")
+    
+    item.cust_id = item_id
+    connection = sqlite3.connect("db.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("UPDATE items SET name=?, price=? WHERE id=?;", (item.name, item.price, item_id))
+    total_changes = connection.total_changes
+    connection.commit()
+    connection.close()
+
+    if total_changes == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    connection = sqlite3.connect("db.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM items WHERE id=? ", (item_id))
+    total_changes = connection.total_changes
+    connection.commit()
+    connection.close()
+
+    if total_changes != 1:
+        raise HTTPException(status_code=400, detail="{total_changes} rows were affected")
+    return total_changes
+
+#####Orders 
+
+"""class Order(BaseModel):
+    item_id: int | None = None
+    cust_id: int | None = None
+    order_id: int | None = None
+    timestamp: int
+    notes: string
+"""
+"""@app.post("/orders/")
+def create_order(order: Order):
+    if order.order_id != None:
+        raise HTTPException(status_code=400, detail="order_id can't be set on post request")
+    
+    connection = sqlite3.connect("db.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO orders (order_id, item_id, cust_id, timestamp, notes) VALUES (?, ?, ?, ?, ?);",(order.order_id, order.item_id, order.cust_id, order.timestamp, order.notes))
+    order.order_id = cursor.lastrowid
+    connection.commit()
+    connection.close()
+
+    return order
+"""
+"""
+@app.get("/orders/{order_id}")
+def read_order(order_id: int, order: Order):
+    connection = sqlite3.connect("db.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("SELECT id FROM orders WHERE order_id= ?", (order_id,))
+    order = cursor.fetchone()
+    connection.close()
+
+    if order != None:
+            return Order (order_id=order[0], item_id=order[1], cust_id=order[2], timestamp=order[3], notes=order[4])       
+    else:
+         raise HTTPException(status_code=404, detail="Order not found")
+"""  
+"""
+@app.put("/orders/{order_id}")
+def update_order(order_id: int, order: Order):
+    if order.order_id != None and order.order_id != order_id:
+        raise HTTPException(status_code=404, detail="Order ID doesn't match ID in Path")
+    
+    order.order_id = order_id
+    connection = sqlite3.connect("db.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("UPDATE orders SET item_id=?, timestamp=?, notes=? WHERE id=?;", (order.item_id, order.timestamp, order.notes, order_id))
+    total_changes = connection.total_changes
+    connection.commit()
+    connection.close()
+
+    if total_changes == 0:
+        raise HTTPException(status_code=404, detail="Order not found")
+    return order
+"""
+"""
+@app.delete("/orders/{order_id}")
+def delete_order(order_id: int):
+    connection = sqlite3.connect("db.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM orders WHERE id=? ", (order_id))
+    total_changes = connection.total_changes
+    connection.commit()
+    connection.close()
+
+    if total_changes != 1:
+        raise HTTPException(status_code=400, detail="{total_changes} rows were affected")
+    return total_changes
+"""
